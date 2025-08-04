0dae4d3fe2d7aa2369d3f96a02798e18a02a7215
yuvarajbryan <yuvaraj31v@gmail.com>
2025-08-04 17:56:33 +0530

    fixcvv

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from .views import HealthCheckView, RootAPIView
 
-app_name = 'core'
-
 urlpatterns = [
     path('', RootAPIView.as_view(), name='api-root'),
-    path('core/health/', HealthCheckView.as_view(), name='health-check'),
+    path('api/health/', HealthCheckView.as_view(), name='health-check'),
 ]
